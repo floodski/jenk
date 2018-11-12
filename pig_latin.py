a9477b3f3c7fdf14e6743fbd78103c24383289a6
floodski <jflo015@aucklanduni.ac.nz>
2018-11-13 11:49:17 +1300

    Now works properly for strings

--- pig_latin.py
+++ pig_latin.py
@@ -1,9 +1,16 @@
 p = "Input a string to be Translated into pig latin:\n"
 original=raw_input(p)
 
-translated = original[1:(len(original))] + original[0]+"ay"
+tokenised = original.split(' ')
 
-if original[0].isupper():
-    translated = translated.lower().capitalize()
+out = ""
 
-print(translated)
+for words in tokenised:
+    translated = words[1:(len(original))] + words[0]+"ay"
+
+    if words[0].isupper():
+        translated = translated.lower().capitalize()
+        
+    out += translated + ' '
+    
+print(out)
